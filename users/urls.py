0d9159fdd249aa6b6bf403b67cd5f0ca42137d83
mahmoudalkirm90 <mahmoudalkirm90@gmail.com>
2026-04-05 14:07:47 +0300

    append to last commit

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 from .views import UserLoginView 
 urlpatterns = [
-   path('otp/verify', verify_otp, name='verify-otp'),
    path('login/', UserLoginView.as_view(), name='user-login'),
    path('otp/resend', ResendOtpView.as_view(), name='resend-otp'),
    path('otp/verify', VerifyOtpView.as_view(), name='verify-otp'),
--- users/views.py
+++ users/views.py
@@ -8,9 +8,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import (UserLoginSerializer,
                            ResendOtpSerializer,
                            VerifyOtpSerializer)
-from django.utils import timezone
-from .models import User
-from .mail_sender import send_email 
 
 class UserLoginView(generics.GenericAPIView):
     serializer_class = UserLoginSerializer
@@ -25,28 +22,6 @@ class UserLoginView(generics.GenericAPIView):
 #     "code": "123456",
 #     "email": "example@afiete.com"
 # }
-
-@api_view(['POST'])
-def verify_otp(request):
-    code = request.data.get('code')
-    email = request.data.get('email')
-    otps = Otp.objects.filter(is_used=False, expires_at__gt=timezone.now()).order_by('-created_at')
-    for otp in otps:
-         if check_password(code, otp.code):
-            otp.is_used = True
-            otp.save()
-            user = otp.user
-            user.is_verified = True
-            user.save()
-            # Generate JWT token
-            refresh = RefreshToken.for_user(user)
-            return Response({"message": "OTP verified successfully"
-                             ,"refresh": str(refresh),
-                               "access": str(refresh.access_token)},
-                              status=200)
-    
-    return Response({"message": "Invalid OTP or email"}, status=400)
-
 class ResendOtpView(generics.GenericAPIView):
     serializer_class = ResendOtpSerializer
 
